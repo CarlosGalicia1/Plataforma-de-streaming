fdede32222d5d4a85e3dde952a425251b4668634
CarlosGalicia1 <111411033+CarlosGalicia1@users.noreply.github.com>
2023-06-02 20:13:49 -0600

    Update Serie.h

--- Serie.h
+++ Serie.h
@@ -1,28 +1,58 @@
 #ifndef SERIE_H
 #define SERIE_H
-#include "Video.h"
-#include "Episode.h"
 #include <iostream>
 #include <vector>
 
 class Serie: public Video{
     private:
         vector<Episode> vect;
-        //Episode episodio[];
     public:
         //Constructor:
-        Serie(int id, int largo, float rate, string nombre, string genero,  vector<Episode> _vect);
+        Serie(int id, int largo, float rate, string nombre, string genero, bool restringido, vector<Episode> episodios);
         //===============setters=======================
-        vector<Episode> getEpisodes();                
+        vector<Episode> getEpisodes();    
+        //===============metodos=======================
+        void addEpisode(Episode _ep);
+        void deleteEpisode(int episodio);
+        string reproducir() override;  
+        string reproducir(int episodio); 
 };
 
-Serie::Serie(int id, int largo, float rate, string nombre, string genero,  vector<Episode> _vect):Video(id,largo,rate,nombre,genero){    
-    vect = _vect;
+Serie::Serie(int id, int largo, float rate, string nombre, string genero,bool restringido, vector<Episode> episodios){ 
+    ID = id;
+    length = largo;
+    rating = rate;
+    name = nombre;
+    genre = genero;
+    restricted = restringido;
+    vect = episodios;
 }
 
 // El metodo getEpisodes retorna un vector de elementos de tipo Episode
 vector<Episode> Serie::getEpisodes(){
     return vect;
+}
+
+void Serie::addEpisode(Episode _ep){
+    vect.push_back(_ep);
+}
+
+void Serie::deleteEpisode(int episodio){
+    while (episodio < 1 || episodio > size(vect))
+    {
+        cout <<"Esta serie contiene "<< to_string(size(vect)) << " episodios, ingrese un numero de episodio a eliminar valido: ";
+        cin >> episodio;
+    }
+    vect.erase(vect.begin()+episodio-1);
+}
+
+string Serie::reproducir(){
+    return "Reproduciendo serie " + this->getName()+ "\nReproduciento capitulo 1: " + vect[0].getName() + "\n";
+}
+
+string Serie::reproducir(int episodio){
+    string s1;
+    return "Reproduciendo serie " + this->getName()+ "\n" + vect[episodio-1].reproducir() + "\n";
 }
 
 #endif
