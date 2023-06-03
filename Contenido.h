cdd739365912842584c9d75c94590a0b3a438a38
CarlosGalicia1 <111411033+CarlosGalicia1@users.noreply.github.com>
2023-06-03 12:53:42 -0600

    Update Contenido.h

--- Contenido.h
+++ Contenido.h
@@ -1,78 +1,78 @@
-#ifndef VIDEO_H
-#define VIDEO_H
+    #ifndef CONTENIDO_H
+    #define CONTENIDO_H
 
-class Video {
-    protected:
-        int ID, length;
-        float rating;
-        string name, genre;
-        bool restricted;
+    class Contenido {
+        protected:
+            int ID, length;
+            float rating;
+            string name, genre;
+            bool restricted;
 
-    public:
-        // ==============getters====================
-        int getID();
-        string getName();
-        int getLength();
-        string getGenre();
-        float getRating();
-        bool getRestricted();
-        //===============setters=======================
-        void setID(int id);
-        void setName(string nombre);
-        void setLength(int largo);
-        void setGenre(string genero);
-        void setRating(float rate);
-        void setRestricted(bool restringido);
-        //===============metodos=======================
-        virtual string reproducir()=0;
-};
+        public:
+            // ==============getters====================
+            int getID();
+            string getName();
+            int getLength();
+            string getGenre();
+            float getRating();
+            bool getRestricted();
+            //===============setters=======================
+            void setID(int id);
+            void setName(string nombre);
+            void setLength(int largo);
+            void setGenre(string genero);
+            void setRating(float rate);
+            void setRestricted(bool restringido);
+            //===============metodos=======================
+            virtual string reproducir()=0;
+    };
 
-int Video::getID(){
-    return ID;
-}
+    int Contenido::getID(){
+        return ID;
+    }
 
-string Video::getName(){
-    return name;
-}
+    string Contenido::getName(){
+        return name;
+    }
 
-int Video::getLength(){
-    return length;
-}
+    int Contenido::getLength(){
+        return length;
+    }
 
-string Video::getGenre(){
-    return genre;
-}
+    string Contenido::getGenre(){
+        return genre;
+    }
 
-float Video::getRating(){
-    return rating;
-}
+    float Contenido::getRating(){
+        return rating;
+    }
 
-bool Video::getRestricted(){
-    return restricted;
-}
+    bool Contenido::getRestricted(){
+        return restricted;
+    }
 
-void Video::setID(int id){
-    ID = id;
-}
+    void Contenido::setID(int id){
+        ID = id;
+    }
 
-void Video::setName(string nombre){
-    name = nombre;
-}
+    void Contenido::setName(string nombre){
+        name = nombre;
+    }
 
-void Video::setLength(int largo){
-    length = largo;
-}
+    void Contenido::setLength(int largo){
+        length = largo;
+    }
 
-void Video::setGenre(string genero){
-    genre = genero;
-}
+    void Contenido::setGenre(string genero){
+        genre = genero;
+    }
 
-void Video::setRating(float rate){
-    rating = rate;
-}
+    void Contenido::setRating(float rate){
+        rating = rate;
+    }
 
-void Video::setRestricted(bool restringido){
-    restricted = restringido;
-}
+    void Contenido::setRestricted(bool restringido){
+        restricted = restringido;
+    }
 
-#endif
+    #endif
