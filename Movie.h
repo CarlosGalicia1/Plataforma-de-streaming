c0d1b5500f79a707c4e25a9f09ffe6f8b057772e
CarlosGalicia1 <111411033+CarlosGalicia1@users.noreply.github.com>
2023-06-02 20:16:21 -0600

    Add files via upload

---
+++ Movie.h
@@ -0,0 +1,29 @@
+#ifndef MOVIE_H
+#define MOVIE_H
+
+class Movie: public Video{
+    public:
+    //Constructor:
+    Movie();
+    Movie(int id, int largo, float rate, string nombre, string genero, bool restringido);
+    //===============metodos=======================
+    string reproducir() override;
+};
+
+Movie::Movie():Video(){
+}
+
+Movie::Movie(int id, int largo, float rate, string nombre, string genero, bool restringido){
+    ID = id;
+    length = largo;
+    rating = rate;
+    name = nombre;
+    genre = genero;
+    restricted = restringido;
+}
+
+string Movie::reproducir(){
+    return "Reproduciendo pelicula " + this->getName() + "\n";
+}
+
+#endif
