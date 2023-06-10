10ba25c40d3f6497849df1409580ba5b59814f3c
CarlosGalicia1 <111411033+CarlosGalicia1@users.noreply.github.com>
2023-06-09 19:06:56 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,102 +1,232 @@
+/*Plataforma de streaming
+El programa actual simula una plataforma de streaming como Netflix, Disney+, Steam o HBO, recabando informacion sobre los usuarios
+existentes de la cuenta como su nombre y su edad para desplegar contenido restringido o apto para todos. Además de esto, el
+programa generará el contenido disponible en clases como películas, videojuegos, series y episodios mediante una archivo que 
+contiene a los anteriores. Finalmente el usuario, dependiendo de su edad, podrá reproducir diversos contenidos al seleccionarlos 
+mediante un menú interactivo que puede controlar con las flechas. Cada clas de contenido en el menu tendrá información relevante 
+como un ID, nombre, duración, genero, rating, y otro tipo de informacion basado en su clase o características únicas.
+*/
+//Bibliotecas
+#include <typeinfo>
+#include <math.h>
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
 #include <windows.h>
+#include <typeindex>
+
+//Clases
 using namespace std;
+#include "Usuario.h"
 #include "Contenido.h"
 #include "Episode.h"
 #include "Serie.h"
-#include "Usuario.h"
 #include "Videojuego.h"
 #include "Movie.h"
 
-int main(){
-    //==========Usuario==========
-    Usuario user("Charly", 18);
-    user.setEdad(99);
-    user.setName("Juan");
-    cout << user.getName()<< endl;
-    cout << user.getEdad() << endl;
+// ======================================================Funciones=============================================================
 
-    //==========Videojuego=======
-    Videojuego game(1, 10, 1.5, "Zelda", "Accion", false, 0, 0, false, 0);
-    game.setID(99);
-    game.setName("Mario");
-    game.setLength(99);
-    game.setGenre("Plataformas");
-    game.setRating(5);
-    game.setRestricted(true);
-    game.setMonedas(99);
-    game.setNivel(99);
-    game.setPowerup(true);
-    game.setSalud(99);
-    cout << game.getID() << endl;
-    cout << game.getName() << endl;
-    cout << game.getLength() << endl;
-    cout << game.getGenre() << endl;
-    cout << game.getRating() << endl;
-    cout << game.getRestricted() << endl;
-    cout << game.getMonedas() << endl;
-    cout << game.getNivel() << endl;
-    cout << game.getPowerup() << endl;
-    cout << game.getSalud() << endl;
-    cout << game.reproducir() << endl;
-    
-    //===========Episodio==========
-    Episode ep(2, 60, 3.5, "Amanecer", "Terror", 2, false);
-    ep.setID(99);
-    ep.setName("Un buen dia");
-    ep.setLength(99);
-    ep.setGenre("Plataformas");
-    ep.setRating(5);
-    ep.setRestricted(true);
-    ep.setSeason(99);
-    cout << ep.getID() << endl;
-    cout << ep.getName() << endl;
-    cout << ep.getLength() << endl;
-    cout << ep.getGenre() << endl;
-    cout << ep.getRating() << endl;
-    cout << ep.getRestricted() << endl;
-    cout << ep.getSeason() << endl;
-    cout << ep.reproducir() << endl;
+template<typename Value, typename Allocator>
+bool Menu(string msg, vector<Value, Allocator> usuarios, vector<Contenido*> vec, bool restricted){
+    /*
+    (Implemento polimorfismo y clases abstractas de manera correcta y util,impemento sobrecarga y sobreescritura de métodos de 
+    manera correcta y util)
+    recibe:"msg" valor de tipo string, "usuarios" vector de valores de tipo universal hasta que se declaren, "vec" vector de
+    valores de tipo Contenido*, "restricted" valor booleano.
+    Despliega un menu interactivo con los vectores de usuarios y contenidos multimedia disponibles, dependiendo de la edad del
+    usuario. El usuario puede navegar en el menu con las flechas y elegir la opcion con la tecla enter
+    devuelve: un valor de tipo booleano
+    */
+    while(GetAsyncKeyState(VK_RETURN) != 0){ //Espera mientras que ninguna tecla este presionada
+		Sleep(1);
+	}
+    vector<string> menu;
+    if(usuarios.size() > 0){ //Si la funcion no recibe un vector vacio de usuarios, se recolectan los usuarios disponibles
+        for (int i = 0; i < usuarios.size();i++){
+            menu.push_back(usuarios[i]->getName());
+        }
+    }
+    else{//Si la funcion recibe un vector vacio de usuarios, se recolectan los contenidos multimedia disponibles
+        vector<Contenido*> newvec;
+        if (restricted == true){ //Se recolectan los contenidos no restringidos si el usuario es menor de edad
+            for (int i = 0; i < vec.size();i++){
+                if(vec[i]->getRestricted() == false){
+                    newvec.push_back(vec[i]);
+                }
+            }
+            vec = newvec;
+        }
+        for (int i = 0; i < vec.size();i++){ // Se crea un string por cada contenido multimedia con sus datos relevantes
+            stringstream stream;
+            string texto;
+            int largo;
+            stream << fixed << setprecision(2) << vec[i]->getRating();
 
-    //=========Serie==============
-    vector<Episode> episodios= {ep};
-    Serie s(3, 4, 4.2, "The walking dead", "Zombies", false, episodios);
-    s.setID(50);
-    s.setName("Suits");
-    s.setLength(10);
-    s.setGenre("Drama");
-    s.setRating(3.7);
-    s.setRestricted(true);
-    Episode ep2(7, 78, 3.3, "Caida", "Accion", 1, true);
-    s.addEpisode(ep2);
-    s.deleteEpisode(2);
-    cout << s.getID() << endl;
-    cout << s.getName() << endl;
-    cout << s.getLength() << endl;
-    cout << s.getGenre() << endl;
-    cout << s.getRating() << endl;
-    cout << s.getRestricted() << endl;
-    vector<Episode> episodes = s.getEpisodes();
-    cout << episodes[0].getName() << endl;
-    cout << s.reproducir() << endl;
-    cout << s.reproducir(1) << endl;
+            //downcasting para acceder al metodo "getlength" especifico de la clase "Serie"
+            const std::type_info& type_info = typeid(*vec[i]) ; 
+            if(type_info == typeid(Serie)) 
+            {   
+                Serie *p1 = (Serie *) vec[i];
+                largo = p1->getLength();
+            }
+            else{
+                largo = vec[i]->getLength();
+            }
 
-    //=========Movie=============
-    Movie p(39, 150, 4.9,"Inception", "Drama", true);
-    p.setID(500);
-    p.setName("Guardianes de la galaxia Vol.3");
-    p.setLength(210);
-    p.setGenre("Accion");
-    p.setRating(5.0);
-    p.setRestricted(false);
-    cout << p.getID() << endl;
-    cout << p.getName() << endl;
-    cout << p.getLength() << endl;
-    cout << p.getGenre() << endl;
-    cout << p.getRating() << endl;
-    cout << p.getRestricted() << endl;
-    cout << p.reproducir() << endl;
+            texto = vec[i]->getName() + "        || ID: " + to_string(vec[i]->getID()) + ", Duracion: "+ to_string(largo)\
+            + ", Genero: " + vec[i]->getGenre() + ", Rating: " + stream.str();
+            
+        
+            if(type_info == typeid(Videojuego))// downcasting para acceder a los métodos especificos de la clase videojuego
+            {   
+                Videojuego *p1 = (Videojuego *) vec[i];
+                texto = texto + ", Monedas: " +  to_string(p1->getMonedas()) + ", Nivel: " + to_string(p1->getNivel()) + ", Powerup: "\
+                + to_string(p1->getPowerup()) + ", Salud: " + to_string(p1->getSalud());
+            }
+
+            texto = texto  +" ||";
+            menu.push_back(texto); //se añade el string del contenido multimedia a un vector de strings
+        }
+
+    }
+   
+    for (int i = 0; i < menu.size();i++){
+        cout << menu[i] << endl;
+    }
+    menu.push_back("Salir");
+	int pointer = 0;
+
+	while(true){
+		system("cls");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+		cout <<"=======================\n" << msg +"\n\n";
+		// ============ color inicial de la primera opcion del menu ===========
+		for (int i = 0; i < menu.size(); ++i){
+			if (i == pointer){
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+				cout << menu[i] << endl;
+			}
+			else{
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+				cout << menu[i] << endl;
+			}
+		}
+		
+		while(true){	
+			//Si la tecla "arriba" es presionada..
+			if (GetAsyncKeyState(VK_UP) != 0){
+				pointer -= 1;
+				if (pointer == -1){
+					pointer = menu.size()-1;
+				}
+				break;
+			}
+			//Si la tecla "abajo" es presionada..
+			else if (GetAsyncKeyState(VK_DOWN) != 0){
+				pointer += 1;
+				if (pointer == menu.size()){
+					pointer = 0;
+				}
+				break;
+			}
+			//Si la tecla "enter" es presionada..
+			else if (GetAsyncKeyState(VK_RETURN) != 0){
+				if (pointer == menu.size()-1){
+                    if (menu[menu.size()-1] == "Salir"){ //Si se da enter en "Salir", el programa termina o retrocede al menu anterior
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+                    cout << "\n\n\n Hasta luego!";
+                    Sleep(500);
+					return 0;
+                    }
+
+                }
+                else{
+                    if(usuarios.size() <= 0){//Si se da enter en un contenido, se reproduce
+                        cout << vec[pointer]->reproducir() << endl;
+                        Sleep(1000);
+                        break;
+                    }
+                    //Si se da click en un usuario se muestran sus contenidos disponibles
+                    else if(typeid(usuarios)== typeid(vector<Usuario*>)){ 
+                        cout << "\n\n\n Hola de nuevo " << menu[pointer]<< "!";
+                        Sleep(500);    
+                        restricted = usuarios[pointer]->getRestricted();
+                        msg = "Peliculas,series y videojuegos";
+                        vector<Usuario*> empty;
+                        Menu(msg, empty, vec, restricted);
+                        break;
+                    }
+                    break;
+                }
+			}
+		}
+		Sleep(150);
+	}
     return 0;
 }
+
+//================================================Funcion principal main ======================================================
+
+int main(){
+    /*(Implemento herencia de manera correcta y util, Implemento polimorfismo y clases abstractas de manera correcta y util)
+    recibe: ningun valor
+    Abre el archivo que tiene los contenidos multimedia a mostrar, los cuales se guardan en vectores de tipo
+    apuntador de diferentes clases y se pasan como parámetros a la funcion "Menu" para ser mostrados en pantalla.
+    devuelve: no devuelve ningun valor
+    */
+
+    string name;
+    int edad;
+    int opcion;
+
+    //Se abre archivo de texto con los contenidos que se guardaran en un vector de tipo Contenido*
+    ifstream file;
+    string lines;
+    vector<Contenido*> vecContenido;
+
+    file.open("contenidos.txt");
+    vector<Episode*> episodios;
+
+    while (getline(file, lines)) {
+        string s;
+        stringstream ss(lines);
+        vector <string> line; 
+        while (getline(ss, s, ',')) {
+            line.push_back(s);
+        }    
+
+        if(s == "MOVIE"){
+            vecContenido.insert(vecContenido.end(),1, new Movie(stoi(line[0]),stoi(line[1]),stof(line[2]),line[3],line[4],stoi(line[5])));
+        }
+
+        if(s == "EPISODE"){
+            episodios.insert(episodios.end(),1,new Episode(stoi(line[0]),stoi(line[1]),stof(line[2]),line[3],line[4],stoi(line[5])));
+        }
+
+        if(s == "SERIE"){
+            vecContenido.insert(vecContenido.end(),1, new Serie(stoi(line[0]),stoi(line[1]),stof(line[2]),line[3],line[4],stoi(line[5]),episodios));
+            episodios.clear();
+        }
+
+        if(s == "JUEGO"){
+        vecContenido.insert(vecContenido.end(),1,new Videojuego(stoi(line[0]),stoi(line[1]),stof(line[2]),line[3],line[4],stoi(line[5]),\
+            stoi(line[6]),stoi(line[7]),stoi(line[8]),stoi(line[9])));
+        }
+    }
+    file.close();
+    
+    //=========================Usuarios========================
+    vector<Usuario*> usuarios = {new Usuario("Charly", 19), new Usuario("Timmy", 9)};
+
+    //==========================Menu=============================
+    string msg = "Usuarios: ";
+    bool restricted = false;
+    Menu(msg, usuarios, vecContenido, restricted);
+
+    return 0;
+
+}
+
